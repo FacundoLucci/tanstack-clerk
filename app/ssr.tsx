b3c1ed731db149ae7ad7ff8230430b22792f3628
FacundoLucci <92830973+FacundoLucci@users.noreply.github.com>
2024-11-23 23:26:41 -0600

    Initial commit

---
+++ app/ssr.tsx
@@ -0,0 +1,18 @@
+// app/ssr.tsx
+/// <reference types="vinxi/types/server" />
+import {
+    createStartHandler,
+    defaultStreamHandler,
+  } from '@tanstack/start/server'
+  import { getRouterManifest } from '@tanstack/start/router-manifest'
+  
+  import { createRouter } from './router'
+  import { createClerkHandler } from '@clerk/tanstack-start/server'
+
+  
+  export default createClerkHandler(
+    createStartHandler({
+        createRouter,
+        getRouterManifest,
+    }),
+  )(defaultStreamHandler)
